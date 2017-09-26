444ee55bb9a34f77598442e1900f6e9097a636a8
clintfidel <clintfide@icloud.com>
2017-09-26 15:47:29 +0100

    configure server file

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import logger from 'morgan';
 import bodyParser from 'body-parser';
 import winston from 'winston';
 import Sequelize from 'sequelize';
+import UserRoutes from './server/routes/user';
+import validator from 'express-validator';
+import RecipeRoutes from './server/routes/recipe';
 
 const app = express();
 
@@ -11,9 +14,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(validator());
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
+app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1/recipes', RecipeRoutes);
+
+app.get('*', (req, res) => {
+    res.send("Wrong routes")
 })
 
 const port = process.env.PORT || 8000;
