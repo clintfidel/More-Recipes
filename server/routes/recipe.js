eb5d17e70d9e43ae88d22bc1540240bf7a112cc4
clintfidel <clintfide@icloud.com>
2017-10-18 16:09:02 +0100

    implement feedback(use object destructuring to reduce code length)

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -1,85 +1,73 @@
 import express from 'express';
 import RecipeController from '../controllers/recipe';
-import Validation from '../middlewares/Validation';
-import Authorization from '../middlewares/Authorization';
+import { checkUserInput, verifyUserId, verifyUserIdExist, verifyRecipeId,
+  recipeNameExist, verifyRecipe, checkReviewsInput, downVote, upVote } from '../middlewares/Validation';
+import { isLoggedIn, } from '../middlewares/Authorization';
 
 const app = express.Router();
 
 // Add recipes route
 app.route('/')
-  .post(Authorization.isLoggedIn, Validation.checkUserInput, RecipeController.addRecipe);
+  .post(isLoggedIn, checkUserInput, recipeNameExist, RecipeController.addRecipe);
 
 
 // Get all Recipes
 app.route('/')
-  .get(Authorization.isLoggedIn, RecipeController.getRecipe);
+  .get(isLoggedIn, RecipeController.getRecipe);
 
 // Delete a recipe
 app.route('/:recipeId')
-  .delete(Authorization.isLoggedIn, RecipeController.deleteARecipe);
+  .delete(isLoggedIn, RecipeController.deleteARecipe);
 
 // Modify a recipe
 app.route('/:recipeId')
-  .put(Authorization.isLoggedIn, RecipeController.modifyRecipe);
+  .put(isLoggedIn, RecipeController.modifyRecipe);
 
 
 // Add favourite recipe
 app.route('/favourites/:recipeId')
-  .post(
-    Authorization.isLoggedIn,
-    RecipeController.favouriteRecipe
-  );
+  .post(isLoggedIn, RecipeController.favouriteRecipe);
 
 // Get favourite recipe
 app.route('/favourites/:userId')
-  .get(
-    Authorization.isLoggedIn,
-    RecipeController.getFavouriteRecipes
-  );
+  .get(isLoggedIn, RecipeController.getFavouriteRecipes);
 
 
 // upVote a recipe
 app.route('/upvote/:recipeId')
-  .post(
-    Authorization.isLoggedIn, Validation.upVote,
-    RecipeController.upVoteRecipe
-  );
+  .post(isLoggedIn, upVote, RecipeController.upVoteRecipe);
 
 // downVote a recipe
 app.route('/downvote/:recipeId')
-  .post(Authorization.isLoggedIn, Validation.downVote, RecipeController.downVoteRecipe);
+  .post(isLoggedIn, downVote, RecipeController.downVoteRecipe);
 
 // get all upvote
 app.route('/upvote/:recipeId')
-  .post(Authorization.isLoggedIn, Validation.downVote, RecipeController.getUpVoteRecipe);
+  .post(isLoggedIn, downVote, RecipeController.getUpVoteRecipe);
 
 // get all downvote
 app.route('/downvote/:recipeId')
-  .post(Authorization.isLoggedIn, Validation.downVote, RecipeController.getDownVoteRecipe);
+  .post(isLoggedIn, downVote, RecipeController.getDownVoteRecipe);
 
 // get all upvote
 app.route('/upvote/:recipeId')
-  .post(Authorization.isLoggedIn, Validation.downVote, RecipeController.getUpVoteRecipe);
+  .post(isLoggedIn, downVote, RecipeController.getUpVoteRecipe);
 
 // get all upvote in descending order
 app.route('/recipes?sort=upvotes&order=descending')
-  .get(Authorization.isLoggedIn, Validation.downVote, RecipeController.getUpVoteRecipe);
+  .get(isLoggedIn, downVote, RecipeController.getUpVoteRecipe);
 
 
 // review a recipe
 app.route('/reviews/:recipeId')
   .post(
-    Authorization.isLoggedIn, Validation.checkReviewsInput, Validation.verifyRecipeId,
-    Validation.verifyUserIdExist,
-    Validation.verifyRecipe, Validation.verifyUserId, RecipeController.reviewRecipe
+    isLoggedIn, checkReviewsInput, verifyRecipeId, verifyUserIdExist,
+    verifyRecipe, verifyUserId, RecipeController.reviewRecipe
   );
 
 // view recipes
 app.route('/views/:recipeId')
-  .post(
-    Authorization.isLoggedIn, Validation.verifyRecipe,
-    RecipeController.viewRecipes
-  );
+  .post(isLoggedIn, verifyRecipe, RecipeController.viewRecipes);
 
 
 export default app;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,22 +1,24 @@
 import express from 'express';
 import UserController from '../controllers/user';
-import Authorization from '../middlewares/Authorization';
+import { checkUserInput, signUpNotification, validateLogin, isSignedUpWithUsername,
+  isSignedUpWithEmail, isLoggedIn, isAdmin } from '../middlewares/Authorization';
 
 const app = express.Router();
 
 // Sign up route
 app.route('/signup')
-  .post(Authorization.checkUserInput, Authorization.isSignedUpWithEmail,
-    Authorization.isSignedUpWithUsername,
-    Authorization.signUpNotification,
-    UserController.signUp);
+  .post(
+    checkUserInput, isSignedUpWithEmail, isSignedUpWithUsername,
+    signUpNotification,
+    UserController.signUp
+  );
 // Sign in route
 app.route('/signin')
-  .post(Authorization.validateLogin, UserController.login);
+  .post(validateLogin, UserController.login);
 
 // get all user Route
 app.route('/')
-  .get(Authorization.isLoggedIn, Authorization.isAdmin, UserController.getAllUsers);
+  .get(isLoggedIn, isAdmin, UserController.getAllUsers);
 
 
 export default app;
